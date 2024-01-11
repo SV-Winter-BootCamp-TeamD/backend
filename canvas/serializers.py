33f0f190342ca844013bc0204daa5fc1067c969b
Yejiin21 <kyg9802@naver.com>
2024-01-11 21:16:54 +0900

    fix: CanvasCreateSerializer 삭제

--- canvas/serializers.py
+++ canvas/serializers.py
@@ -1,12 +1,6 @@
 from rest_framework import serializers
 from .models import Canvas
 
-class CanvasCreateSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Canvas
-        fields = ['canvas_name', 'owner_id']
-
 class CanvasSerializer(serializers.ModelSerializer):
 
     class Meta:
