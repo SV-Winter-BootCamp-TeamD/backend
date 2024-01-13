00679548a8cfa93a2a935da951dce91f1ed3d7e9
Yejiin21 <kyg9802@naver.com>
2024-01-14 00:01:39 +0900

    feat: 개인 캔버스 전체 조회 API 구현

--- canvas/views.py
+++ canvas/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 
 from user.models import User
 from .models import Canvas, CanvasMember
-from .serializers import CanvasSerializer
+from .serializers import CanvasSerializer, CanvasPersonalListSerializer
 
 class CanvasCreateView(APIView):
     def post(self, request, *args, **kwargs):
@@ -80,3 +80,24 @@ class MemberInvite(APIView):
         return Response({'message': '친구 초대에 실패했습니다.',
                                  'result': None}, status=status.HTTP_404_NOT_FOUND)
 
+class CanvasPersonalListView(APIView):
+    def get(self, request, user_id):
+        canvases = Canvas.objects.filter(owner_id=user_id)
+        serializer = CanvasPersonalListSerializer(canvases, many=True)
+
+        canvas_list = []
+        for canvas in canvases:
+            canvas_data = {
+                "canvas_id": canvas.id,
+                "canvas_preview_url": canvas.canvas_preview_url,
+                "canvas_name": canvas.canvas_name,
+                "update_at": canvas.updated_at,
+            }
+            canvas_list.append(canvas_data)
+
+        return Response({
+            "message": "개인 캔버스 전체 조회 성공",
+            "result": {
+                "canvases": canvas_list
+            }
+        }, status=status.HTTP_200_OK)
