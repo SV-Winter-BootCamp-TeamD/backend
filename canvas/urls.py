100cd89b0414d49a50e8322de210577c7beea373
Yejiin21 <kyg9802@naver.com>
2024-01-14 16:02:58 +0900

    feat: 개인 캔버스 전체 조회 API 구현

--- canvas/urls.py
+++ canvas/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('<int:canvas_id>/stickers/ai/select/', StickerSelectView.as_view(), name='sticker-ai-select'),
     path('<int:canvas_id>/<int:component_id>/', ComponentDeleteView.as_view(), name='component-delete'),
     path('<int:canvas_id>/save/', CanvasSaveView.as_view(), name='canvas-save'),
+    path('<int:canvas_id>/backgrounds/recommend/', BackgroundRecommendView.as_view(), name='background-recommend'),
     path('personal/<int:user_id>/', CanvasPersonalListView.as_view(), name='canvas-list')
 ]
