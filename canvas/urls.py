18ea1e045d7385ad71913f1e8818dfd5f77942c1
MJJ <m3k0813@gmail.com>
2024-01-14 21:17:52 +0900

    fix: 친구 초대 API 수정

--- canvas/urls.py
+++ canvas/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import CanvasCreateView, CanvasUpdateDeleteView, MemberInvite, CanvasSaveView, CanvasPersonalListView
+from .views import CanvasCreateView, CanvasUpdateDeleteView, MemberInviteView, CanvasSaveView, CanvasPersonalListView
 from component.views import BackgroundUploadView, BackgroundAIView, BackgroundSelectView, StickerAIView, StickerSelectView, TextUploadView, ComponentDeleteView, BackgroundRecommendView
 
 
 urlpatterns = [
     path('', CanvasCreateView.as_view(), name='create-canvas'),
     path('<int:canvas_id>/', CanvasUpdateDeleteView.as_view(), name='update/delete-canvas'),
-    path('<int:canvas_id>/invite/', MemberInvite.as_view(), name='invite-canvas'),
+    path('<int:canvas_id>/invite/', MemberInviteView.as_view(), name='invite-canvas'),
     path('<int:canvas_id>/backgrounds/upload/', BackgroundUploadView.as_view(), name='background-upload'),
     path('<int:canvas_id>/texts/upload/', TextUploadView.as_view(), name='text-upload'),
     path('<int:canvas_id>/backgrounds/ai/', BackgroundAIView.as_view(), name='background-ai'),
