061d7d08b7442211633d484bd330be2a284b1fec
MJJ <m3k0813@gmail.com>
2024-01-14 22:35:52 +0900

    feat: 웹 소켓 연결

--- canvas/urls.py
+++ canvas/urls.py
@@ -5,7 +5,7 @@ from component.views import BackgroundUploadView, BackgroundAIView, BackgroundSe
 
 urlpatterns = [
     path('', CanvasCreateView.as_view(), name='create-canvas'),
-    path('<int:canvas_id>/', CanvasUpdateDeleteView, name='update/delete-canvas'),
+    path('<int:canvas_id>/', CanvasUpdateDeleteView.as_view(), name='update/delete-canvas'),
     path('<int:canvas_id>/invite/', MemberInviteView.as_view(), name='invite-canvas'),
     path('<int:canvas_id>/', CanvasUpdateDeleteView.as_view(), name='update/delete-canvas'),
     path('<int:canvas_id>/invite/', MemberInviteView.as_view(), name='invite-canvas'),
